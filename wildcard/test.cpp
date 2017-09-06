924bff92fbe94b21463c1bcbaebc1229169e5294
trumanzhao <trumanzhao@foxmail.com>
2017-09-07 01:05:14 +0800

    增加通配匹配函数

---
+++ wildcard/test.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "wildcard.h"
+
+void do_test(const char* text, const char* pattern, bool expected_match, const char* expected_output)
+{
+	char* my_text = strdup(text);
+	const char* match_res = wildcard_match(text, pattern) == expected_match ? "OK" : "Failed";
+
+	int replace_count = 0;
+	while (wildcard_replace(my_text, pattern, replace_count, '*') && replace_count > 0)
+	{
+	}
+	const char* replace_res = strcmp(my_text, expected_output) == 0 ? "OK" : "Failed";
+	printf("\"%s\":\"%s\"-->%s|%s\n", text, pattern, match_res, replace_res);
+	free(my_text);
+}
+
+int main()
+{
+	do_test("", "", true, "");
+	do_test("abc", "", false, "abc");
+	do_test("abc", "abc", true, "***");
+	do_test("abc", "bc", false, "abc");
+	do_test("abc", "*", true, "abc");
+	do_test("abc", "?bc", true, "a**");
+	do_test("abc", "ab?", true, "**c");
+	do_test("abc", "*bc", true, "a**");
+	do_test("abc", "ab*", true, "**c");
+	do_test("abc", "a*c", true, "*b*");
+	do_test("abc", "a*bc", true, "***");
+	do_test("abc", "a?bc", false, "abc");
+	return 0;
+}
---
+++ wildcard/wildcard.h
@@ -0,0 +1,61 @@
+#pragma once
+
+template <typename char_type>
+bool wildcard_match(const char_type* text, const char_type* pattern)
+{
+    if (*pattern == '?')
+    {
+        return wildcard_match(text + 1, pattern + 1);
+    }
+
+    if (*pattern == '*')
+    {
+        do
+        {
+            if (wildcard_match(text, pattern + 1))
+                return true;
+        } while (*text++ != '\0');
+
+        return false;
+    }
+
+    return *pattern == *text && (*text == '\0' || wildcard_match(text + 1, pattern + 1));
+}
+
+template <typename char_type>
+bool wildcard_replace(char_type* text, const char_type* pattern, int& replace_count, char_type replace_char)
+{
+    if (*pattern == '?')
+    {
+        return wildcard_replace(text + 1, pattern + 1, replace_count, replace_char);
+    }
+
+    if (*pattern == '*')
+    {
+        do
+        {
+            if (wildcard_replace(text, pattern + 1, replace_count, replace_char))
+                return true;
+        } while (*text++ != '\0');
+
+        return false;
+    }
+
+    if (*pattern == *text)
+    {
+		if (*text == '\0')
+		{
+			replace_count = 0;
+			return true;
+		}
+
+		if (wildcard_replace(text + 1, pattern + 1, replace_count, replace_char))
+		{
+			*text = replace_char;
+			++replace_count;
+			return true;
+		}
+    }
+
+    return false;
+}
